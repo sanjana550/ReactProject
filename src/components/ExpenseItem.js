b06c8657e5cf7d6e95b7e88ad848408fbeca4152
Sanjana <sanjanamondaltffs@gmail.com>
2023-08-06 12:01:02 +0530

    first

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 1021</div>
+      <div>
+        <h2>Car Insurence</h2>
+        <div>Rs 100</div>
+        <h2>Food</h2>  
+        <div>Rs 10</div>
+<h2>Petrol</h2>
+<div>Rs 100</div>
+<h2>Movies</h2>
+<div>Rs 200</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
